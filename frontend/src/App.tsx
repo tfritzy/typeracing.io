f09ecbd4d427192a563663e501da19eefcf47442
Tyler <tfritzy@gmail.com>
2024-04-04 18:28:37 -0700

    WIP barebones frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,61 @@ import {
   encodeFindGameRequest,
   OneofRequest,
   encodeOneofRequest,
+  decodeOneofUpdate,
 } from "./compiled";
 
 function App() {
   const [ws, setWs] = React.useState<WebSocket | null>(null);
+  const [token, setToken] = React.useState<string>("");
+  const [phrase, setPhrase] = React.useState<string | undefined>();
+  const [players, setPlayers] = React.useState<string[]>([]);
 
   React.useEffect(() => {
-    var ws = new WebSocket("ws://localhost:5000/");
+    const token = "tkn_" + Math.random().toString(36).substring(7);
+    setToken(token);
+    var ws = new WebSocket(`ws://localhost:5000/?token=${token}`);
     ws.onopen = () => {
       console.log("Connected");
     };
     ws.onmessage = (event) => {
-      console.log(event.data);
+      if (event.data === null) {
+        console.log("Received null data");
+      } else if (event.data instanceof Blob) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.result instanceof ArrayBuffer) {
+            const buffer = new Uint8Array(reader.result);
+            const update = decodeOneofUpdate(buffer);
+            console.log("Decoded update", update);
+
+            if (update.game_starting) {
+              console.log("game starting");
+              setPhrase(update.game_starting.phrase);
+            } else if (update.player_joined_game) {
+              console.log(update.player_joined_game?.player_name);
+              setPlayers((players) => [
+                ...players,
+                update.player_joined_game?.player_name || "<unknown>",
+              ]);
+            }
+          }
+        };
+        reader.readAsArrayBuffer(event.data);
+      }
+      // reader.readAsArrayBuffer(event.data);
+      // console.log("Received", event);
+      // const update = decodeOneofUpdate(new Uint8Array(event.data));
+      // console.log("Decoded update", update);
+      // if (update.game_starting) {
+      //   console.log("game starting");
+      //   setPhrase(update.game_starting.phrase);
+      // } else if (update.player_joined_game) {
+      //   console.log(update.player_joined_game?.player_name);
+      //   setPlayers((players) => [
+      //     ...players,
+      //     update.player_joined_game?.player_name || "<unknown>",
+      //   ]);
+      // }
     };
     ws.onclose = () => {
       console.log("Disconnected");
@@ -37,13 +80,17 @@ function App() {
     player_name: "Jeff",
   };
   const request: OneofRequest = {
-    sender_id: "plyr_123",
+    sender_id: token,
     find_game: findGame,
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        {players.map((player) => (
+          <p key={player}>{player}</p>
+        ))}
+        {phrase}
         <input type="text" onChange={handleInput} />
         <button onClick={() => ws?.send(encodeOneofRequest(request))}>
           Find Game
--- src/messaging/Server.cs
+++ src/messaging/Server.cs
@@ -1,25 +1,61 @@
 using System.Net;
 using System.Net.WebSockets;
+using Google.Protobuf;
 
 namespace LightspeedTyperacing;
 
 public class Server
 {
-    public Dictionary<Guid, WebSocket> Connections { get; set; }
+    public Dictionary<string, WebSocket> Connections { get; set; }
     public Galaxy Galaxy { get; set; }
 
     public Server()
     {
-        Connections = new Dictionary<Guid, WebSocket>();
+        Connections = new Dictionary<string, WebSocket>();
         Galaxy = new Galaxy();
+        StartProcessOutboxTask();
+    }
+
+    void StartProcessOutboxTask()
+    {
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                try
+                {
+                    await ProcessOutbox();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to process outbox: " + ex.Message);
+                }
+            }
+        }).ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                Console.WriteLine("Restarting ProcessOutbox task...");
+                StartProcessOutboxTask();
+            }
+        }, TaskContinuationOptions.OnlyOnFaulted);
     }
 
     public async Task AcceptConnection(HttpListenerContext context)
     {
         WebSocketContext webSocketContext = null;
+        var token = context.Request.QueryString["token"];
+        if (token == null)
+        {
+            context.Response.StatusCode = 400;
+            context.Response.Close();
+            return;
+        }
+
         try
         {
             webSocketContext = await context.AcceptWebSocketAsync(subProtocol: null);
+            Connections[token] = webSocketContext.WebSocket;
             Console.WriteLine($"WebSocket connection established at {context.Request.Url}");
         }
         catch (Exception e)
@@ -31,10 +67,10 @@ public class Server
         }
 
         WebSocket webSocket = webSocketContext.WebSocket;
-        _ = Task.Run(() => ListenLoop(webSocket));
+        _ = Task.Run(() => ListenLoop(webSocket, token));
     }
 
-    private async void ListenLoop(WebSocket webSocket)
+    private async void ListenLoop(WebSocket webSocket, string token)
     {
         byte[] receiveBuffer = new byte[1024];
         while (webSocket.State == WebSocketState.Open)
@@ -42,15 +78,17 @@ public class Server
             var receiveResult = await webSocket.ReceiveAsync(
                 new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
 
-            Console.WriteLine(
-                $"Received message of type {receiveResult.MessageType} with length {receiveResult.Count}");
+            int messageLength = receiveResult.Count;
 
             if (receiveResult.MessageType == WebSocketMessageType.Binary)
             {
                 try
                 {
-                    OneofRequest request = OneofRequest.Parser.ParseFrom(receiveBuffer);
-                    HandleRequest(request);
+                    using (var ms = new MemoryStream(receiveBuffer, 0, messageLength))
+                    {
+                        OneofRequest request = OneofRequest.Parser.ParseFrom(ms);
+                        HandleRequest(request);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -61,13 +99,41 @@ public class Server
             else if (receiveResult.MessageType == WebSocketMessageType.Close)
             {
                 Console.WriteLine("WebSocket connection closed by client.");
+                Connections.Remove(token);
                 await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+            }
+        }
+    }
+
+    public async Task ProcessOutbox()
+    {
+        while (Galaxy.Outbox.Count > 0)
+        {
+            OneofUpdate update = Galaxy.Outbox.Dequeue();
+            if (
+                Connections.ContainsKey(update.RecipientId) &&
+                Connections[update.RecipientId].State == WebSocketState.Open)
+            {
+                WebSocket webSocket = Connections[update.RecipientId];
+                Console.WriteLine($"Sending update to {update.RecipientId} of type {update.UpdateCase}");
+                byte[] data = update.ToByteArray();
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(data, 0, data.Length),
+                    WebSocketMessageType.Binary,
+                    true,
+                    CancellationToken.None);
+            }
+            else
+            {
+                Console.WriteLine($"Failed to send update to {update.RecipientId}");
             }
         }
     }
 
     public void HandleRequest(OneofRequest request)
     {
+        Console.WriteLine($"Received request of type {request.RequestCase} from {request.SenderId}");
+
         switch (request.RequestCase)
         {
             case OneofRequest.RequestOneofCase.FindGame:
