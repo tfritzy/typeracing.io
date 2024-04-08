203e5b14d2a79afa153cea58bf746fc6f4242e6c
Tyler <tfritzy@gmail.com>
2024-04-08 09:05:47 -0700

    Mostly complete home page

--- frontend/src/StartGame.tsx
+++ frontend/src/StartGame.tsx
@@ -1,90 +1,134 @@
 import React from "react";
-import {
- FindGameRequest,
- OneofRequest,
- encodeOneofRequest,
-} from "./compiled";
+import { FindGameRequest, OneofRequest, encodeOneofRequest } from "./compiled";
 import { FullPhraseTypeBox } from "./FullPhraseTypeBox";
+import { DesignPencil, GraduationCap, Group, Lock } from "iconoir-react";
 
 type StartGameProps = {
- token: string;
- playerName: string;
- setPlayerName: (name: string) => void;
- sendRequest: (request: ArrayBuffer) => void;
+  token: string;
+  playerName: string;
+  setPlayerName: (name: string) => void;
+  sendRequest: (request: ArrayBuffer) => void;
 };
 
 export enum Mode {
- Practice,
- Multiplayer,
+  Practice,
+  Multiplayer,
 }
 
 enum State {
- Menu,
- EnteringStardate,
- Engage,
+  Menu,
+  EnteringStardate,
+  Engage,
 }
 
 export const StartGame = (props: StartGameProps) => {
- const [state, setState] = React.useState<State>(
-  State.Menu
- );
+  const [state, setState] = React.useState<State>(State.Menu);
 
- const findGame: FindGameRequest = {
-  player_name: props.playerName,
- };
- const request: OneofRequest = {
-  sender_id: props.token,
-  find_game: findGame,
- };
+  const findGame: FindGameRequest = {
+    player_name: props.playerName,
+  };
+  const request: OneofRequest = {
+    sender_id: props.token,
+    find_game: findGame,
+  };
 
- let content;
- if (state === State.Menu) {
-  content = (
-   <div className="flex flex-col space-y-16">
-    <FullPhraseTypeBox
-     key="join race"
-     phrase="Enter race"
-     onPhraseComplete={() => {
-      setState(State.EnteringStardate);
-     }}
-    />
-   </div>
+  let content;
+  if (state === State.Menu) {
+    content = (
+      <div className="border border-neutral-100">
+        <div className="text-3xl w-full bg-neutral-100 text-neutral-800 px-4 py-2">
+          Lightspeed Typeracing
+        </div>
+
+        <div className="flex flex-col">
+          <div className="px-4 pb-6 pt-4 flex flex-col">
+            <div className="mb-5">
+              <div className="text-md font-semibold flex flex-row space-x-1 items-center">
+                <div>Multiplayer</div>
+                <Group height={16} width={16} />
+              </div>
+              <div className="text-sm text-neutral-200">
+                Race against players across the galaxy
+              </div>
+            </div>
+
+            <button
+              className="border border-neutral-100 text-neutral-100 px-2 py-2 bg-neutral-900 font-bold flex flex-row justify-center items-center space-x-2"
+              onClick={() => setState(State.EnteringStardate)}
+            >
+              <div>Join race</div>
+            </button>
+          </div>
+
+          <div className="w-full border-t border-neutral-100" />
+
+          <div className="flex flex-col">
+            <div className="px-4 pb-6 pt-4 flex flex-col">
+              <div className="mb-3">
+                <div className="text-md font-semibold flex flex-row space-x-1 items-center">
+                  <div>Practice</div>
+                  <GraduationCap height={16} width={16} />
+                </div>
+                <div className="text-sm text-neutral-200">
+                  Race by your self to improve your typing skills
+                </div>
+              </div>
+
+              <button
+                className="border border-neutral-100 text-neutral-100 px-2 py-2 bg-neutral-900 font-bold"
+                onClick={() => setState(State.EnteringStardate)}
+              >
+                Start practice
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full border-t border-neutral-100" />
+
+          <div className="flex flex-col">
+            <div className="px-4 pb-6 pt-4 flex flex-col">
+              <div className="mb-3">
+                <div className="text-md font-semibold flex flex-row space-x-1 items-center">
+                  <div>Race against friends</div>
+                  <Lock height={16} width={16} />
+                </div>
+                <div className="text-sm text-neutral-200">
+                  Create a private lobby and invite your friends
+                </div>
+              </div>
+
+              <button
+                className="border border-neutral-100 text-neutral-100 px-2 py-2 bg-neutral-900 font-bold"
+                onClick={() => setState(State.EnteringStardate)}
+              >
+                Create lobby
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (state === State.EnteringStardate) {
+    content = (
+      <FullPhraseTypeBox
+        key="stardate"
+        phrase="Helm: Set course for gemini IV, warp 2"
+        onPhraseComplete={() => setState(State.Engage)}
+      />
+    );
+  } else {
+    content = (
+      <FullPhraseTypeBox
+        key="engage"
+        phrase="Engage."
+        onPhraseComplete={() => props.sendRequest(encodeOneofRequest(request))}
+      />
+    );
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center text-white">
+      <div className="flex flex-col space-y-16">{content}</div>
+    </div>
   );
- } else if (state === State.EnteringStardate) {
-  content = (
-   <FullPhraseTypeBox
-    key="stardate"
-    phrase="Helm: Set course for gemini IV, warp 2"
-    onPhraseComplete={() => setState(State.Engage)}
-   />
-  );
- } else {
-  content = (
-   <FullPhraseTypeBox
-    key="engage"
-    phrase="Engage."
-    onPhraseComplete={() =>
-     props.sendRequest(encodeOneofRequest(request))
-    }
-   />
-  );
- }
-
- return (
-  <div className="w-full h-screen flex flex-col items-center justify-center text-white">
-   <div className="text-4xl">Lightspeed Typeracing</div>
-   <img
-    src="/splash.webp"
-    alt="Ship"
-    className="w-96 h-96"
-   />
-
-   <div>
-    <div className="text-md">In amber clad</div>
-    <img src="/Ship.svg" alt="Ship" className="w-32 h-32" />
-   </div>
-
-   <div className="flex flex-col space-y-16">{content}</div>
-  </div>
- );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'georgia': ['Monaco', 'Menlo', 'Consolas' ],
+        georgia: ["Monaco", "Menlo", "Consolas"],
       },
       keyframes: {
         pulseFull: {
-          '0%, 100%': { opacity: '1' },
-          '50%': { opacity: '0' },
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
         },
       },
       animation: {
-        'pulse-full': 'pulseFull 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        "pulse-full": "pulseFull 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
   plugins: [],
-}
+};
