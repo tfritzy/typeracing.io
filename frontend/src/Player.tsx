ca19fb090becb4256e5583fff993696f20a037d3
Tyler <tfritzy@gmail.com>
2024-04-10 20:29:10 -0700

    Drew ship

--- frontend/src/Player.tsx
+++ frontend/src/Player.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 type PlayerProps = {
- name: string;
- progress: number;
+  name: string;
+  progress: number;
 };
 
 export const Player = (props: PlayerProps) => {
- return (
-  <div className="h-md w-screen px-8">
-   <div className="text-white text-lg">{props.name}</div>
-   <div className="text-gray-300 text-sm">
-    {Math.floor(
-     props.progress * 299_792_458
-    ).toLocaleString()}{" "}
-    m/s
-   </div>
-   <img src="/Ship.svg" alt="Ship" className="w-16 h-16" />
-  </div>
- );
+  return (
+    <div className="h-md w-screen px-8">
+      <div className="text-white text-lg">{props.name}</div>
+      <div className="text-gray-300 text-sm">
+        {Math.floor(props.progress * 299_792_458).toLocaleString()} m/s
+      </div>
+      <img src="/ship.svg" alt="Ship" className="w-32 h-32" />
+    </div>
+  );
 };
--- frontend/src/ShipEditor.tsx
+++ frontend/src/ShipEditor.tsx
@@ -2,32 +2,27 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store/store";
 import { updatePlayerName } from "./store/playerSlice";
-import { EditPencil, Check } from "iconoir-react";
 
 export const ShipEditor = () => {
- const dispatch = useDispatch();
- const playerName = useSelector(
-  (state: RootState) => state.player.name
- );
+  const dispatch = useDispatch();
+  const playerName = useSelector((state: RootState) => state.player.name);
 
- return (
-  <div className="border border-gray-100 h-min">
-   <div className="text-2xl w-full bg-gray-100 text-gray-900 px-4 py-2 uppercase font-semibold">
-    Shipyard
-   </div>
+  return (
+    <div className="border border-neutral-200 h-min">
+      <div className="text-2xl w-full bg-neutral-200 text-neutral-900 px-4 py-2 uppercase font-semibold">
+        Shipyard
+      </div>
 
-   <img src="/Ship.svg" alt="Ship" className="w-64 h-64" />
+      <img src="/ship.svg" alt="Ship" className="w-64 h-64 rotate-45" />
 
-   <div className="p-2">
-    <input
-     type="text"
-     className="w-full bg-transparent border border-gray-100 px-2 py-1"
-     value={playerName}
-     onChange={(e) =>
-      dispatch(updatePlayerName(e.target.value))
-     }
-    />
-   </div>
-  </div>
- );
+      <div className="p-2">
+        <input
+          type="text"
+          className="w-full bg-transparent border border-neutral-200 px-2 py-1"
+          value={playerName}
+          onChange={(e) => dispatch(updatePlayerName(e.target.value))}
+        />
+      </div>
+    </div>
+  );
 };
