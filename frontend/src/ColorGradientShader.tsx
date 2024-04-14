2b35253e53efc8b102debfb6e4a983daa592cb9b
Tyler <tfritzy@gmail.com>
2024-04-13 17:47:09 -0700

    Pog stars

---
+++ frontend/src/ColorGradientShader.tsx
@@ -0,0 +1,30 @@
+export const ColorGradientShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    width: { value: window.innerWidth },
+    height: { value: window.innerHeight },
+    speed: { value: 1.0 }, // New uniform for controlling intensity
+  },
+  vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+  fragmentShader: `
+      uniform sampler2D tDiffuse;
+      uniform float width;  
+      uniform float speed;
+      varying vec2 vUv;
+      void main() {
+        vec4 texel = texture2D(tDiffuse, vUv);
+        float xPosition = vUv.x;
+        vec3 leftColor = vec3(5, 2, 2);
+        vec3 rightColor = vec3(2, 2, 5); // Blue
+        vec3 color = mix(leftColor, rightColor, xPosition);
+        color = mix(vec3(1.0, 1.0, 1.0), color, speed * speed); // Interpolate based on speed
+        gl_FragColor = vec4(texel.rgb * color, texel.a);
+      }
+    `,
+};
--- frontend/src/StarsV2.tsx
+++ frontend/src/StarsV2.tsx
@@ -1,130 +1,163 @@
-// src/components/ThreeCanvas.tsx
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { splitmix32 } from "./helpers/splitmix32";
 import { generateStar } from "./helpers/starGenerator";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { ColorGradientShader } from "./ColorGradientShader";
 
-const starColors = [
- "#f2d3ab",
- "#c69fa5",
- "#fbf5ef",
- "#8b6d9c",
-];
+const starColors = ["#68c2d3", "#a2dcc7"];
+const MAX_SHIP_SPEED = 20000;
+const NUM_STARS = 3000;
 
 const generateStars = (width: number, height: number) => {
- const stars = [];
- const rng = splitmix32(0x12345678);
- for (let i = 0; i < 1000; i++) {
-  const star = generateStar(rng);
-  const size = 1 + star.z * 2;
-  const geometry = new THREE.CircleGeometry(size, 6);
-  geometry.rotateZ(Math.PI / 2);
-  const color =
-   starColors[Math.floor(rng.next() * starColors.length)];
-  const material = new THREE.MeshBasicMaterial({
-   color: color,
-   side: THREE.DoubleSide,
-  });
-  const circle = new THREE.Mesh(geometry, material);
-  circle.position.x = star.x * width;
-  circle.position.y = star.y * height;
-  circle.position.z = star.z;
-  stars.push(circle);
- }
- return stars;
+  const stars = [];
+  const rng = splitmix32(0x12345678);
+  const materials = starColors.map(
+    (color) =>
+      new THREE.MeshBasicMaterial({
+        color: color,
+      })
+  );
+  for (let i = 0; i < NUM_STARS; i++) {
+    const star = generateStar(rng);
+    const size = 1 + star.z * 2;
+    const geometry = new THREE.CircleGeometry(size, 6);
+    geometry.rotateZ(Math.PI / 2);
+    const circle = new THREE.Mesh(
+      geometry,
+      materials[Math.floor(rng.next() * materials.length)]
+    );
+    circle.position.x = star.x * width;
+    circle.position.y = star.y * height;
+    circle.position.z = star.z;
+    stars.push(circle);
+  }
+  return stars;
 };
 
 const ThreeCanvas: React.FC = () => {
- const canvasRef = useRef<HTMLCanvasElement>(null);
- const shipSpeed = useRef<number>(0.5);
+  const [inputSpeed, setInputSpeed] = React.useState(0);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const shipSpeed = useRef<number>(0.5);
 
- useEffect(() => {
-  if (!canvasRef.current) return;
+  useEffect(() => {
+    shipSpeed.current = inputSpeed;
+  }, [inputSpeed]);
 
-  let dpr = window.devicePixelRatio || 1;
-  let width = window.innerWidth * dpr;
-  let height = window.innerHeight * dpr;
-  let stars = generateStars(width, height);
+  // Increment speed over time
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setInputSpeed((prev) => Math.min(1, prev + 0.0001));
+    }, 1);
+    return () => clearInterval(interval);
+  }, []);
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.OrthographicCamera(
-   width / -2,
-   width / 2,
-   height / 2,
-   height / -2,
-   1,
-   1000
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    let dpr = window.devicePixelRatio || 1;
+    let width = window.innerWidth * dpr;
+    let height = window.innerHeight * dpr;
+    let stars = generateStars(width * 1.1, height);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(
+      width / -2,
+      width / 2,
+      height / 2,
+      height / -2,
+      1,
+      1000
+    );
+    camera.position.z = 500; // adjust accordingly
+
+    for (const star of stars) {
+      scene.add(star);
+    }
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true,
+    });
+    renderer.setClearColor("#3a3858");
+    const adjustCanvas = () => {
+      dpr = window.devicePixelRatio || 1;
+      width = window.innerWidth * dpr;
+      height = window.innerHeight * dpr;
+      renderer.setSize(width, height, false);
+      camera.updateProjectionMatrix();
+    };
+    adjustCanvas();
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+    const colorGradientShader = new ShaderPass(ColorGradientShader);
+    composer.addPass(colorGradientShader);
+
+    let lastTime = 0;
+    const render = () => {
+      colorGradientShader.uniforms.speed.value =
+        shipSpeed.current * shipSpeed.current * shipSpeed.current;
+
+      const time = performance.now();
+      const delta_s = (time - lastTime) / 1000;
+      lastTime = performance.now();
+      stars.forEach((star) => {
+        // exponentially map from 0 to MAX_SHIP_SPEED
+        const speed = shipSpeed.current * MAX_SHIP_SPEED * star.position.z;
+        star.scale.x = Math.max(
+          1,
+          shipSpeed.current * shipSpeed.current * star.position.z * 400
+        );
+        star.position.x -= speed * delta_s;
+        if (star.position.x < -width) {
+          star.position.x = width;
+          star.position.y = (Math.random() * 2 - 1) * height;
+        }
+      });
+
+      requestAnimationFrame(render);
+      // renderer.render(scene, camera);
+      composer.render();
+    };
+
+    window.addEventListener("resize", adjustCanvas, false);
+    render();
+
+    return () => {
+      window.removeEventListener("resize", adjustCanvas);
+      scene.clear();
+      // renderer.dispose();
+      composer.dispose();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="fixed left-[50%] bottom-[50%] transform[-translate-x-1/2 translate-y-1/2] z-10">
+        <div className="text-white text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+          {Math.trunc(inputSpeed * 299792).toLocaleString()} km/s
+        </div>
+        {/* <input
+          type="range"
+          min="0"
+          max="1"
+          step=".01"
+          value={inputSpeed}
+          onChange={(e) => setInputSpeed(parseFloat(e.target.value))}
+        /> */}
+      </div>
+      <canvas
+        ref={canvasRef}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </>
   );
-  camera.position.z = 500; // adjust accordingly
-
-  for (const star of stars) {
-   scene.add(star);
-  }
-
-  const renderer = new THREE.WebGLRenderer({
-   canvas: canvasRef.current,
-   antialias: true,
-  });
-  renderer.setClearColor("#272744");
-
-  const adjustCanvas = () => {
-   dpr = window.devicePixelRatio || 1;
-   width = window.innerWidth * dpr;
-   height = window.innerHeight * dpr;
-   renderer.setSize(width, height, false);
-   camera.updateProjectionMatrix();
-  };
-
-  let lastTime = 0;
-  const render = () => {
-   const time = performance.now();
-   const delta_s = (time - lastTime) / 1000;
-   stars.forEach((star) => {
-    const speed =
-     star.position.z * 2 * shipSpeed.current * delta_s;
-    star.scale.x = 1 + speed / 4;
-    star.position.x -= speed;
-    if (star.position.x < -width) {
-     star.position.x = width;
-    }
-   });
-
-   requestAnimationFrame(render);
-   renderer.render(scene, camera);
-  };
-
-  window.addEventListener("resize", adjustCanvas, false);
-  adjustCanvas();
-  render();
-
-  return () => {
-   window.removeEventListener("resize", adjustCanvas);
-   scene.clear();
-   renderer.dispose();
-  };
- }, []);
-
- return (
-  <>
-   <input
-    type="range"
-    min="0"
-    max="1"
-    step=".01"
-    value={shipSpeed.current}
-    onChange={(e) =>
-     (shipSpeed.current = parseFloat(e.target.value))
-    }
-   />
-   <canvas
-    ref={canvasRef}
-    style={{
-     width: "100%",
-     height: "100%",
-    }}
-   />
-  </>
- );
 };
 
 export default ThreeCanvas;
