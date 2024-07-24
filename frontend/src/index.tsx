a85a7df18b89e102bffa06bc2f342aa77f436884
Tyler <tfritzy@gmail.com>
2024-07-24 07:24:03 -0700

    Fixing background

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,27 +5,19 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
-import HexagonGrid from "./HexBackground";
 
 const root = ReactDOM.createRoot(
- document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
- <>
-  <div className="fixed left-0 top-">
-   <HexagonGrid
-    backgroundColor="#a7f3d0"
-    hexagonColor="#0d1117"
-    raceStartTime={10_000}
-   />
-  </div>
-  <div className="text-text-primary">
-   <Provider store={store}>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-   </Provider>
-  </div>
- </>
+  <>
+    <div className="text-text-primary">
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  </>
 );
