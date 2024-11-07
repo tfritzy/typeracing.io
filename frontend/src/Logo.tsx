c944fafa0b358945e46c6d9e97c85c5ff140e67f
Tyler <tfritzy@gmail.com>
2024-11-06 21:27:34 -0600

    Revert logo

--- frontend/src/Logo.tsx
+++ frontend/src/Logo.tsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom";
-
 export const Logo = () => {
   return (
-    <Link
-      to="/"
+    <a
       className="rounded-lg py-2 border border-border-color px-3 flex flex-row font-bold logo w-min h-min bg-neutral-color"
+      href="/"
     >
       <span className="">Type</span>
       <span className="">Racing</span>
       <span className="text-accent-secondary">.io</span>
-    </Link>
+    </a>
   );
 };
