7e6c06bf5b30be6761aa228ab2d32541b06569f7
Tyler <tfritzy@gmail.com>
2024-12-05 06:55:19 -0600

    .

--- frontend/src/main-menu/Profile.tsx
+++ frontend/src/main-menu/Profile.tsx
@@ -2,24 +2,12 @@ import { RootState } from "../store/store";
 import React from "react";
 import { updatePlayerName } from "../store/playerSlice";
 import Cookies from "js-cookie";
-import { calculateWpm } from "../helpers/raceResults";
 import { useAppDispatch, useAppSelector } from "../store/storeHooks";
 import EditInput from "./EditInput";
 
 export const Profile = () => {
-  const [open, setOpen] = React.useState<boolean>(false);
   const dispatch = useAppDispatch();
   const player = useAppSelector((state: RootState) => state.player);
-  const raceResults = useAppSelector(
-    (state: RootState) => state.player.raceResults
-  );
-  const currentMode = useAppSelector(
-    (state: RootState) => state.player.gameMode
-  );
-
-  const wpm = React.useMemo(() => {
-    return calculateWpm(raceResults, currentMode);
-  }, [currentMode, raceResults]);
 
   const updateName = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updatePlayerName(e.target.value));
