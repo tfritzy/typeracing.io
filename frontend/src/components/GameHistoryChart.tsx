f5a71cdfd1d1cd1941a0f9d21c033b675be25f46
Tyler <tfritzy@gmail.com>
2025-02-11 07:25:59 -0600

    WIP

--- frontend/src/components/GameHistoryChart.tsx
+++ frontend/src/components/GameHistoryChart.tsx
@@ -2,11 +2,12 @@ import { useMemo } from "react";
 
 type Props = {
   points: number[][];
+  year: number;
 };
 
-const COLS = 60;
-const ROWS = 20;
-export function GameHistoryChart({ points: raw }: Props) {
+const COLS = 53;
+const ROWS = 16;
+export function GameHistoryChart({ points: raw, year }: Props) {
   const points: number[][] = useMemo(() => {
     if (raw.length < COLS) {
       const paddingStart = Math.floor((COLS - raw.length) / 2);
@@ -20,12 +21,10 @@ export function GameHistoryChart({ points: raw }: Props) {
     return raw;
   }, [raw]);
 
-  console.log("points", points);
-
   const content: JSX.Element[][] = useMemo(() => {
     const squares: number[][] = [];
-    for (let i = 0; i < 60; i++) {
-      squares[i] = Array(20).fill(0);
+    for (let i = 0; i < COLS; i++) {
+      squares[i] = Array(ROWS).fill(0);
     }
 
     let high = 0;
@@ -40,11 +39,9 @@ export function GameHistoryChart({ points: raw }: Props) {
 
     let highestCount = 0;
     for (let day = 0; day < points.length; day++) {
-      console.log(day, points.length);
-      const col = Math.round((day / points.length) * COLS);
+      const col = Math.round((day / points.length) * (COLS - 1));
       for (const point of points[day]) {
         const row = Math.round((point / high) * ROWS);
-        console.log("colrow", col, row);
         squares[col][row] += 1;
         if (squares[col][row] > highestCount) {
           highestCount = squares[col][row];
@@ -58,11 +55,41 @@ export function GameHistoryChart({ points: raw }: Props) {
       }
     }
 
-    console.log(squares);
-
     const cols = [];
+
+    const wpmPerRow = high / ROWS;
+
+    const labels = [];
+    for (let i = ROWS; i >= 0; i--) {
+      if (i !== ROWS && i % 3 === 0) {
+        labels.push(
+          <div className="w-6 h-3 text-xs leading-none text-right">
+            {(wpmPerRow * i).toFixed(0)}
+          </div>
+        );
+      } else {
+        labels.push(<div className="h-3" />);
+      }
+    }
+
+    cols.push(labels);
+
+    const daysPerCol = points.length / COLS;
+
     for (let x = 0; x < squares.length; x++) {
       const col = [];
+
+      const week = new Date(year, 0, 7 * x);
+      if (week.getDate() <= 7) {
+        col.push(
+          <div className="w-3 h-3 whitespace-nowrap text-xs">
+            {week.toLocaleString("en-US", { month: "short" })}
+          </div>
+        );
+      } else {
+        col.push(<div className="w-3 h-3" />);
+      }
+
       for (let y = squares[0].length - 1; y >= 0; y--) {
         col.push(
           <div
@@ -81,5 +108,9 @@ export function GameHistoryChart({ points: raw }: Props) {
     return cols.map((c) => <div className="flex flex-col space-y-1">{c}</div>);
   }, [points]);
 
-  return <div className="flex flex-row gap-1">{content}</div>;
+  return (
+    <div className="flex flex-row items-center gap-1 text-base-500">
+      {content}
+    </div>
+  );
 }
